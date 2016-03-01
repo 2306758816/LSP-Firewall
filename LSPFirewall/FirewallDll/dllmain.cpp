c25bdaa19e4c23c7a6751fd60c68eec40ebd1437
PandaShare <pandahk@vip.qq.com>
2016-03-01 22:57:29 +0800

    add files

--- LSPFirewall/FirewallDll/dllmain.cpp
+++ LSPFirewall/FirewallDll/dllmain.cpp
@@ -1,14 +1,21 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
 
+
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
 					 )
 {
+	ZeroMemory(&g_appInfo, sizeof(g_appInfo));
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+	{
+		g_appInfo.dwCurrentProcessId = GetCurrentProcessId();
+		GetModuleFileNameA(NULL, g_appInfo.szCurrentAppPath, MAX_PATH + 1);
+	}break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
